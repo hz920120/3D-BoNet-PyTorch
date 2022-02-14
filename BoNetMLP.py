592f0ca9f97d89a540b7f89969f3cb1f1db4a9a3
cesc <hz920120@gmail.com>
2022-02-14 12:33:07 +0800

    commit

--- BoNetMLP.py
+++ BoNetMLP.py
@@ -30,6 +30,13 @@ class backbone_pointnet2(nn.Module):
         self.fp2 = PointnetFPModule(mlp=[320, 256, 128])
         self.fp1 = PointnetFPModule(mlp=[137, 128, 128, 128, 128])
         self.is_train = is_train
+        self.conv1 = nn.Conv2d(128, 128, kernel_size=(1, 1))
+        self.lrelu1 = nn.LeakyReLU(negative_slope)
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=(1, 1), stride=(1, 1))
+        self.lrelu2 = nn.LeakyReLU(negative_slope)
+        self.drop = nn.Dropout()
+        self.conv3 = nn.Conv2d(64, 13, kernel_size=(1, 1), stride=(1, 1))
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, xyz, points):
         points_num = xyz.size()[1]
@@ -46,24 +53,42 @@ class backbone_pointnet2(nn.Module):
         point_features = l0_points
 
         # sem
-
+        l0_points = l0_points.transpose(1, 2)
         l0_points = l0_points[:, :, None, :]
-        sem1 = F.leaky_relu(F.conv2d(l0_points, torch.rand(4096, 4096, 1, 1).cuda()),
-                            negative_slope)
-        # sem2 = Ops.xxlu(Ops.conv2d(sem1, k=(1, 1), out_c=64, str=1, pad='VALID', name='sem2'), label='lrelu')
-        sem2 = F.leaky_relu(F.conv2d(sem1, torch.rand(4096, 4096, 1, 1).cuda(), stride=2),
-                            negative_slope)
-        # sem2 = Ops.dropout(sem2, keep_prob=0.5, is_train=self.is_train, name='sem2_dropout')
-        sem2 = F.dropout(sem2, p=0.5, training=self.is_train)
-        # sem3 = Ops.conv2d(sem2, k=(1, 1), out_c=self.sem_num, str=1, pad='VALID', name='sem3')
-        # TODO self.sem_num = 13?
-        sem3 = F.conv2d(sem2, torch.rand(4096, 4096, 1, 4).cuda(), padding=2, stride=5)
-        # TODO points_num ?
-        # sem3 = torch.reshape(sem3, [-1, points_num, self.sem_num])
-        sem4 = torch.reshape(sem3, [-1, points_num, 13])
-        y_sem_pred = torch.squeeze(F.softmax(sem4), dim=1)
+        sem1 = self.lrelu1(self.conv1(l0_points))
+        sem2 = self.lrelu2(self.conv2(sem1))
+        sem2 = self.drop(sem2)
+        sem3 = self.conv3(sem2)
+        sem4 = torch.reshape(sem3.transpose(1, 2), [-1, points_num, 13])
+        y_sem_pred = self.softmax(sem4)
 
         return point_features, global_features, y_sem_pred, sem4
+
+
+# class backbone_sem(nn.Module):
+#     def __init__(self):
+#         super(backbone_sem, self).__init__()
+#         self.conv1 = nn.Conv2d(128, 128, kernel_size=(1, 1))
+#         self.lrelu1 = nn.LeakyReLU(negative_slope)
+#         self.conv2 = nn.Conv2d(128, 64, kernel_size=(1, 1), stride=(1, 1))
+#         self.lrelu2 = nn.LeakyReLU(negative_slope)
+#         self.drop = nn.Dropout()
+#         self.conv3 = nn.Conv2d(64, 13, kernel_size=(1, 1), stride=(1, 1))
+#         self.softmax = nn.Softmax(dim=1)
+#
+#     def forward(self, xyz, l0_points):
+#         # sem
+#         points_num = xyz.size()[1]
+#         l0_points = l0_points.transpose(1, 2)
+#         l0_points = l0_points[:, :, None, :]
+#         sem1 = self.lrelu1(self.conv1(l0_points))
+#         sem2 = self.lrelu2(self.conv2(sem1))
+#         sem2 = self.drop(sem2)
+#         sem3 = self.conv3(sem2)
+#         sem4 = torch.reshape(sem3.transpose(1,2), [-1, points_num, 13])
+#         y_sem_pred = self.softmax(sem4)
+#
+#         return y_sem_pred, sem4
 
 
 # 2. bbox
@@ -77,8 +102,6 @@ class bbox_net(nn.Module):
         self.fc3 = nn.Linear(256, 24 * 2 * 3)
         self.fc4 = nn.Linear(256, 256)
         self.fc5 = nn.Linear(256, 24)
-        # self.bn1= nn.BatchNorm1d(512)
-        # self.bn2= nn.BatchNorm1d(256)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, global_features):
@@ -103,7 +126,7 @@ class bbox_net(nn.Module):
         # sub_branch 2
         b4 = F.leaky_relu(self.fc4(b2), negative_slope=negative_slope)
         # TODO out_dim????? y_bbscore_pred = tf.sigmoid(Ops.fc(b4, out_d=self.bb_num * 1, name='y_bbscore_pred'))
-        y_bbscore_pred = torch.sigmoid(self.fc5(b4))
+        y_bbscore_pred = self.sigmoid(self.fc5(b4))
 
         return y_bbvert_pred, y_bbscore_pred
 
@@ -113,41 +136,42 @@ class pmask_net(nn.Module):
     def __init__(self, p_f_num):
         super(pmask_net, self).__init__()
         self.fc1 = nn.Linear(512, 256)
+        self.lrelu1 = nn.LeakyReLU(negative_slope)
         self.conv1 = nn.Conv2d(1, 256, (1, p_f_num))
+        self.lrelu2 = nn.LeakyReLU(negative_slope)
         self.conv2 = nn.Conv2d(512, 128, (1, 1))
+        self.lrelu3 = nn.LeakyReLU(negative_slope)
         self.conv3 = nn.Conv2d(128, 128, (1, 1))
+        self.lrelu4 = nn.LeakyReLU(negative_slope)
         self.conv4 = nn.Conv2d(1, 64, (1, 135))
+        self.lrelu5 = nn.LeakyReLU(negative_slope)
         self.conv5 = nn.Conv2d(64, 32, (1, 1))
+        self.lrelu6 = nn.LeakyReLU(negative_slope)
         self.conv6 = nn.Conv2d(32, 1, (1, 1))
-        self.bn1 = nn.BatchNorm2d(128)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.bn4 = nn.BatchNorm2d(32)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, point_features, global_features, bbox, bboxscore):
         p_num = point_features.shape[1]
         num_box = bbox.shape[1]
-        global_features = F.leaky_relu(self.fc1(global_features), negative_slope=negative_slope).unsqueeze(1).unsqueeze(
-            1).repeat(1, p_num, 1, 1)
-        point_features = F.leaky_relu(self.conv1(point_features.unsqueeze(-1).permute(0, 3, 1, 2)), negative_slope=0.2)
+        global_features = self.lrelu1(self.fc1(global_features))[:, None, None, :].repeat(1, p_num, 1, 1)
+        point_features = self.lrelu2(self.conv1(point_features.unsqueeze(-1).permute(0, 3, 1, 2)))
         point_features = torch.cat((point_features, global_features.permute(0, 3, 1, 2)), dim=1)
-        point_features = F.leaky_relu(self.conv2(point_features), negative_slope=negative_slope)
-        point_features = F.leaky_relu(self.conv3(point_features), negative_slope=negative_slope)
+        point_features = self.lrelu3(self.conv2(point_features))
+        point_features = self.lrelu4(self.conv3(point_features))
         point_features = point_features.squeeze(-1)
 
         # TODO add bboxscore, TF code is as follows
-        bbox_info = Ops.tile(
+        bbox_info = Ops.repeat(
             torch.cat([torch.reshape(bbox, [-1, num_box, 6]), bboxscore[:, :, None]], dim=-1)[:, :, None, :],
             [1, 1, p_num, 1])
         pmask0 = point_features.transpose(1, 2).unsqueeze(1).repeat(1, num_box, 1, 1)
         pmask0 = torch.cat((pmask0, bbox_info), dim=-1)
-        pmask0 = pmask0.view(-1, p_num, pmask0.shape[-1], 1)
+        pmask0 = pmask0.reshape(-1, p_num, pmask0.shape[-1], 1)
 
-        pmask1 = F.leaky_relu(self.conv4(pmask0.permute(0, 3, 1, 2)), negative_slope=negative_slope)
-        pmask2 = F.leaky_relu(self.conv5(pmask1), negative_slope=negative_slope)
+        pmask1 = self.lrelu5(self.conv4(pmask0.permute(0, 3, 1, 2)))
+        pmask2 = self.lrelu6(self.conv5(pmask1))
         pmask3 = self.conv6(pmask2).permute(0, 2, 3, 1)
-        pmask3 = pmask3.view(-1, num_box, p_num)
+        pmask3 = pmask3.reshape(-1, num_box, p_num)
 
         pred_mask = self.sigmoid(pmask3)
 
@@ -182,14 +206,23 @@ class FocalLoss2(nn.Module):
         self.logits = logits
         self.reduce = reduce
 
-    def forward(self, inputs, targets):
+    def forward(self, X_pc, inputs, targets):
+        points_num = X_pc.size()[1]
+        # valid ins
+        Y_pmask_helper = targets.sum(dim=-1)
+        Y_pmask_helper = Ops.cast(torch.gt(Y_pmask_helper, 0.), torch.float32)
+        Y_pmask_helper = Y_pmask_helper[:, :, None].repeat([1, 1, points_num])
+
+        targets = targets * Y_pmask_helper
+        inputs = inputs * Y_pmask_helper
+
         F_loss = -targets * self.alpha * ((1. - inputs) ** self.gamma) * torch.log(inputs + 1e-8) \
                  - (1. - targets) * (1. - self.alpha) * (inputs ** self.gamma) * torch.log(1. - inputs + 1e-8)
 
-        if self.reduce:
-            return torch.mean(F_loss) * 60
-        else:
-            return F_loss * 60
+        pmask_loss_focal = torch.sum(F_loss * Y_pmask_helper) / torch.sum(Y_pmask_helper)
+        pmask_loss = 30 * pmask_loss_focal
+
+        return pmask_loss
 
 
 class Hungarian(nn.Module):
@@ -223,3 +256,124 @@ class Hungarian(nn.Module):
             loss_total += cost[idx][row_ind, col_ind].sum()
             ordering[idx] = np.reshape(col_ind, [1, -1])
         return torch.from_numpy(ordering).cuda(), (loss_total / float(batch_size * num_instances))
+
+
+class PsemCeLoss(nn.Module):
+    def __init__(self):
+        super(PsemCeLoss, self).__init__()
+
+    def forward(self, inputs, targets):
+        loss = nn.CrossEntropyLoss(reduction='mean')
+        return loss(inputs, torch.argmax(targets, dim=1))
+
+
+class BbVertLoss(nn.Module):
+    def __init__(self, label):
+        super(BbVertLoss, self).__init__()
+        self.label = label
+
+    def forward(self, X_pc, y_bbvert_pred, Y_bbvert):
+        label = self.label
+        points_num = X_pc.size()[1]
+        bb_num = int(Y_bbvert.shape[1])
+        points_xyz = X_pc[:, :, 0:3]
+        points_xyz = Ops.repeat(points_xyz[:, None, :, :], [1, bb_num, 1, 1])
+
+        ##### get points hard mask in each gt bbox
+        gt_bbox_min_xyz = Y_bbvert[:, :, 0, :]
+        gt_bbox_max_xyz = Y_bbvert[:, :, 1, :]
+        gt_bbox_min_xyz = Ops.repeat(gt_bbox_min_xyz[:, :, None, :], [1, 1, points_num, 1])
+        gt_bbox_max_xyz = Ops.repeat(gt_bbox_max_xyz[:, :, None, :], [1, 1, points_num, 1])
+        tp1_gt = gt_bbox_min_xyz - points_xyz
+        tp2_gt = points_xyz - gt_bbox_max_xyz
+        tp_gt = tp1_gt * tp2_gt
+        points_in_gt_bbox_prob = Ops.cast(
+            torch.eq(torch.mean(Ops.cast(torch.ge(tp_gt, 0.), torch.float32), dim=-1),
+                     torch.tensor([1.0], device=torch.device('cuda'))),
+            torch.float32)
+
+        ##### get points soft mask in each pred bbox
+        pred_bbox_min_xyz = y_bbvert_pred[:, :, 0, :]
+        pred_bbox_max_xyz = y_bbvert_pred[:, :, 1, :]
+        pred_bbox_min_xyz = Ops.repeat(pred_bbox_min_xyz[:, :, None, :], [1, 1, points_num, 1])
+        pred_bbox_max_xyz = Ops.repeat(pred_bbox_max_xyz[:, :, None, :], [1, 1, points_num, 1])
+        tp1_pred = pred_bbox_min_xyz - points_xyz
+        tp2_pred = points_xyz - pred_bbox_max_xyz
+        tp_pred = 100 * tp1_pred * tp2_pred
+        tp_pred = torch.max(torch.min(tp_pred, torch.empty(tp_pred.shape, device=torch.device('cuda')).fill_(20.)),
+                            torch.empty(tp_pred.shape, device=torch.device('cuda')).fill_(-20.0))
+        # tp_pred = torch.maximum(torch.minimum(tp_pred, torch.tensor(20.0)), torch.tensor(-20.0))
+        points_in_pred_bbox_prob = 1.0 / (1.0 + torch.exp(-1.0 * tp_pred))
+        points_in_pred_bbox_prob = torch.min(points_in_pred_bbox_prob, dim=-1).values
+
+        ##### helper -> the valid bbox (the gt boxes are zero-padded during data processing, pickup valid ones here)
+        Y_bbox_helper = torch.sum(torch.reshape(Y_bbvert, [-1, bb_num, 6]), dim=-1)
+        Y_bbox_helper = Ops.cast(torch.gt(Y_bbox_helper, 0.), torch.float32)
+
+        ##### 1. get ce loss of valid/positive bboxes, don't count the ce_loss of invalid/negative bboxes
+        Y_bbox_helper_tp1 = Ops.repeat(Y_bbox_helper[:, :, None], [1, 1, points_num])
+        bbox_loss_ce_all = -points_in_gt_bbox_prob * torch.log(points_in_pred_bbox_prob + (1e-8)) \
+                           - (1. - points_in_gt_bbox_prob) * torch.log(1. - points_in_pred_bbox_prob + 1e-8)
+        bbox_loss_ce_pos = torch.sum(bbox_loss_ce_all * Y_bbox_helper_tp1) / torch.sum(Y_bbox_helper_tp1)
+        bbox_loss_ce = bbox_loss_ce_pos
+
+        ##### 2. get iou loss of valid/positive bboxes
+        TP = torch.sum(points_in_pred_bbox_prob * points_in_gt_bbox_prob, dim=-1)
+        FP = torch.sum(points_in_pred_bbox_prob, dim=-1) - TP
+        FN = torch.sum(points_in_gt_bbox_prob, dim=-1) - TP
+        bbox_loss_iou_all = TP / (TP + FP + FN + 1e-6)
+        bbox_loss_iou_all = -1.0 * bbox_loss_iou_all
+        bbox_loss_iou_pos = torch.sum(bbox_loss_iou_all * Y_bbox_helper) / torch.sum(Y_bbox_helper)
+        bbox_loss_iou = bbox_loss_iou_pos
+
+        ##### 3. get l2 loss of both valid/positive bboxes
+        bbox_loss_l2_all = (Y_bbvert - y_bbvert_pred) ** 2
+        bbox_loss_l2_all = torch.mean(torch.reshape(bbox_loss_l2_all, [-1, bb_num, 6]), dim=-1)
+        bbox_loss_l2_pos = torch.sum(bbox_loss_l2_all * Y_bbox_helper) / torch.sum(Y_bbox_helper)
+
+        ## to minimize the 3D volumn of invalid/negative bboxes, it serves as a regularizer to penalize false pred bboxes
+        ## it turns out to be quite helpful, but not discussed in the paper
+        bbox_pred_neg = Ops.repeat((1. - Y_bbox_helper)[:, :, None, None], [1, 1, 2, 3]) * y_bbvert_pred
+        bbox_loss_l2_neg = (bbox_pred_neg[:, :, 0, :] - bbox_pred_neg[:, :, 1, :]) ** 2
+        bbox_loss_l2_neg = torch.sum(bbox_loss_l2_neg) / (torch.sum(1. - Y_bbox_helper) + 1e-8)
+
+        bbox_loss_l2 = bbox_loss_l2_pos + bbox_loss_l2_neg
+
+        #####
+        if label == 'use_all_ce_l2_iou':
+            bbox_loss = bbox_loss_ce + bbox_loss_l2 + bbox_loss_iou
+        elif label == 'use_both_ce_l2':
+            bbox_loss = bbox_loss_ce + bbox_loss_l2
+        elif label == 'use_both_ce_iou':
+            bbox_loss = bbox_loss_ce + bbox_loss_iou
+        elif label == 'use_both_l2_iou':
+            bbox_loss = bbox_loss_l2 + bbox_loss_iou
+        elif label == 'use_only_ce':
+            bbox_loss = bbox_loss_ce
+        elif label == 'use_only_l2':
+            bbox_loss = bbox_loss_l2
+        elif label == 'use_only_iou':
+            bbox_loss = bbox_loss_iou
+        else:
+            bbox_loss = None
+            print('bbox loss label error!')
+            exit()
+
+        return bbox_loss, bbox_loss_l2, bbox_loss_ce, bbox_loss_iou
+
+
+class BbScoreLoss(nn.Module):
+    def __init__(self):
+        super(BbScoreLoss, self).__init__()
+
+    def forward(self, y_bbscore_pred, Y_bbvert):
+        bb_num = int(Y_bbvert.shape[1])
+
+        ##### helper -> the valid bbox
+        Y_bbox_helper = torch.sum(torch.reshape(Y_bbvert, [-1, bb_num, 6]), dim=-1)
+        Y_bbox_helper = Ops.cast(torch.gt(Y_bbox_helper, 0.), torch.float32)
+
+        ##### bbox score loss
+        bbox_loss_score = torch.mean(-Y_bbox_helper * torch.log(y_bbscore_pred + 1e-8)
+                                     - (1. - Y_bbox_helper) * torch.log(1. - y_bbscore_pred + 1e-8))
+        return bbox_loss_score
